cd251bc65ceef51e95dbbb0d31d7c1c125c4a99b
BRazvann <razvanbornac_98@yahoo.com>
2020-07-31 10:35:47 +0300

    updated scrapyd

--- scrapyd/tests/test_environ.py
+++ scrapyd/tests/test_environ.py
@@ -26,14 +26,14 @@ class EnvironmentTest(unittest.TestCase):
         slot = 3
         env = self.environ.get_environment(msg, slot)
         self.assertEqual(env['SCRAPY_PROJECT'], 'mybot')
-        self.assertEqual(env['SCRAPY_SLOT'], '3')
-        self.assertEqual(env['SCRAPY_SPIDER'], 'myspider')
-        self.assertEqual(env['SCRAPY_JOB'], 'ID')
-        self.assert_(env['SCRAPY_LOG_FILE'].endswith(os.path.join('mybot', 'myspider', 'ID.log')))
+        self.assertEqual(env['SLOT'], '3')
+        self.assertEqual(env['SPIDER'], 'myspider')
+        self.assertEqual(env['JOB'], 'ID')
+        self.assert_(env['LOG_FILE'].endswith(os.path.join('mybot', 'myspider', 'ID.log')))
         if env.get('SCRAPY_FEED_URI'):  # Not compulsory
             self.assert_(env['SCRAPY_FEED_URI'].startswith('file://{}'.format(os.getcwd())))
             self.assert_(env['SCRAPY_FEED_URI'].endswith(os.path.join('mybot', 'myspider', 'ID.jl')))
-        self.assertNotIn('SCRAPY_SETTINGS_MODULE', env)
+        self.failIf('SCRAPY_SETTINGS_MODULE' in env)
 
     def test_get_environment_with_no_items_dir(self):
         config = Config(values={'items_dir': '', 'logs_dir': ''})
@@ -43,5 +43,5 @@ class EnvironmentTest(unittest.TestCase):
         slot = 3
         environ = Environment(config, initenv={})
         env = environ.get_environment(msg, slot)
-        self.assertNotIn('SCRAPY_FEED_URI', env)
-        self.assertNotIn('SCRAPY_LOG_FILE', env)
+        self.failUnless('SCRAPY_FEED_URI' not in env)
+        self.failUnless('LOG_FILE' not in env)
